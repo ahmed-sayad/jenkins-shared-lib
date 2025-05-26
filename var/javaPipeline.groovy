e9cd624247f210ee089e79d65d5dbbdcda6b52a4
Ahmed Elsayad <ahmed@example.com>
2025-05-26 15:37:56 +0300

    Update pipleine

---
+++ var/javaPipeline.groovy
@@ -0,0 +1,54 @@
+def run() {
+    pipeline {
+        agent any
+
+        environment {
+            MAVEN_HOME = tool 'M3'
+            PATH = "${MAVEN_HOME}/bin:${env.PATH}"
+        }
+
+        stages {
+            stage('Clone') {
+                steps {
+                    echo 'Cloning done by Jenkins'
+                }
+            }
+
+            stage('Build the project') {
+                steps {
+                    sh 'mvn clean install'
+                }
+            }
+
+            stage('Test the project') {
+                steps {
+                    script {
+                        try {
+                            sh 'mvn test'
+                        } catch (err) {
+                            echo "Tests failed or not found, continuing pipeline"
+                        }
+                    }
+                }
+            }
+
+            stage('Docker Build') {
+                steps {
+                    script {
+                        dockerImage = docker.build("ahmedelsayad/java-app")
+                    }
+                }
+            }
+
+            stage('Docker Push') {
+                steps {
+                    script {
+                        withDockerRegistry(credentialsId: 'docker-hub-creds', url: '') {
+                            dockerImage.push()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
